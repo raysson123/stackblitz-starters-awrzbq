c2cb1a6b9f1faa688ba187713bc4535b709ad1d2
raysson123 <44059139+raysson123@users.noreply.github.com>
2023-08-08 13:14:33 -0300

    5 files modified, 11 added, 3 deleted

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsComponent } from './components/products/products.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { CoupensComponent } from './components/coupens/coupens.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { MediaComponent } from './components/media/media.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'coupens', component: CoupensComponent },
+  { path: 'pages', component: PagesComponent },
+  { path: 'media', component: MediaComponent },
+  { path: 'settings', component: SettingsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { SideNavToggle } from './interfaces/side-nav-toggle';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'PainelEstoque';
+
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void {
+    this.isSideNavCollapsed = data.collapsed;
+    this.screenWidth = data.screenWidth;
+  }
+}
